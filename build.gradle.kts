dd6687e377c01f91ad3b1fe90e153e9e02f9a03f
gvdenbro <gregory.vandenbroucke@gmail.com>
2019-10-02 20:43:16 +0200

    git revision

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
+    kotlin("jvm") version "1.3.41"
 }
 
 group = "be.gvdenbro"
-version = "0.0.26"
+version = gitDescribeVersion()
 
 repositories {
     jcenter()
@@ -17,4 +19,25 @@ dependencies {
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+fun gitDescribeVersion(): String {
+
+    val stdOut = ByteArrayOutputStream()
+
+    exec {
+        commandLine("git", "describe", "--tags", "--long", "--always", "--match", "[0-9].[0-9]*")
+        standardOutput = stdOut
+        workingDir = rootDir
+    }
+
+    val gitDescribeMatchRegex = """(\d+)\.(\d+)-(\d+)-.*""".toRegex()
+    val describe = stdOut.toString().trim()
+
+    return gitDescribeMatchRegex.matchEntire(describe)
+            ?.destructured
+            ?.let { (major, minor, patch) ->
+                "$major.$minor.$patch"
+            }
+            ?: throw GradleException("Cannot parse git describe '$describe'")
 }
