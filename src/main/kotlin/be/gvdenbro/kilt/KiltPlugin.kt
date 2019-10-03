f657322c88272d622822ef4f484c39b4b2b3aee2
gvdenbro <gregory.vandenbroucke@gmail.com>
2019-10-03 09:58:50 +0200

    adding basic plugin

--- src/main/kotlin/be/gvdenbro/kilt/KiltPlugin.kt
+++ src/main/kotlin/be/gvdenbro/kilt/KiltPlugin.kt
@@ -3,18 +3,48 @@ package be.gvdenbro.kilt
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-open class KiltPluginExtension {
-    var git: KiltPluginExtensionGit = KiltPluginExtensionGit()
+import java.net.URL
+
+open class KiltConfig {
+    var git = KiltGitConfig()
+    var slack = KiltSlackConfig()
+    var gocd = KiltGocdConfig()
+    var mergeDetails = LinkedHashMap<String, String>()
 }
 
-open class KiltPluginExtensionGit {
-    var userName: String? = null
-    var userEmail: String? = null
+open class KiltGitConfig {
+    lateinit var userName: String
+    lateinit var userEmail: String
 }
 
+open class KiltSlackConfig {
+    lateinit var hookURL: URL
+}
+
+open class KiltGocdConfig {
+    lateinit var url: URL
+}
+
+/**
+ * this plugin integrates GoCD/Git/Slack into one whole. It is not meant to be reusable.
+ */
 class KiltPlugin : Plugin<Project> {
 
-    override fun apply(target: Project) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun apply(project: Project) {
+
+        project.run {
+
+            val config = extensions.create("kilt", KiltConfig::class.java)
+
+            config.mergeDetails.forEach { source, destination ->
+
+                tasks.register("merge$source$destination", GitMergeTask::class.java) {
+                    it.source = source
+                    it.destination = destination
+                    it.userName = config.git.userName
+                    it.userEmail = config.git.userEmail
+                }
+            }
+        }
     }
 }
